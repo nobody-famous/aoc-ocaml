70051347abd1f1a71652445c4223e58e5bae886f
Rich Heller <rellehr@gmail.com>
2021-04-20 13:55:37 -0500

    Parser updates, toying with language syntax

--- lib/2019/2/part1.ml
+++ lib/2019/2/part1.ml
@@ -1,5 +1,27 @@
 open Parser
+open Printf
 
 let run file_name =
-  parse_input file_name;
+  let prog = parse_input file_name in
+
+  let print_array a =
+    let rec helper ndx =
+      match ndx with
+      | n when ndx < Array.length a ->
+          printf ",%d" a.(n);
+          helper (n + 1)
+      | _ -> printf "\n"
+    in
+
+    match Array.length a with
+    | 0 -> ()
+    | _ ->
+        printf "%d" a.(0);
+        helper 1
+  in
+
+  print_array prog;
+  let ndx = 3 in
+  let a, b, c = (prog.(ndx), prog.(ndx + 1), prog.(ndx + 2)) in
+  printf "%d,%d,%d\n" a b c;
   0
