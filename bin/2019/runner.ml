c6011de0982b7e7536b0c4272d17955b1f29cfa2
Rich Heller <rellehr@gmail.com>
2021-04-22 22:39:06 -0500

    Part 2 solution

--- bin/2019/runner.ml
+++ bin/2019/runner.ml
@@ -34,6 +34,12 @@ let fns : problem list =
       fn = Aoc_2019_3.Part1.run;
       exp = 529;
     };
+    {
+      name = "2019.3.2";
+      file = "input/2019/3/puzzle.txt";
+      fn = Aoc_2019_3.Part2.run;
+      exp = 20386;
+    };
   ]
 
 let timed_run (p : problem) =
@@ -46,6 +52,6 @@ let timed_run (p : problem) =
   printf "%s took %d ms\n" p.name diff
 
 ;;
-(* List.iter timed_run fns *)
+List.iter timed_run fns
 
-Aoc_2019_3.Part2.run "input/2019/3/sample.txt"
+(* Aoc_2019_3.Part2.run "input/2019/3/puzzle.txt" *)
--- lib/2019/3/part2.ml
+++ lib/2019/3/part2.ml
@@ -1,4 +1,3 @@
-open Printf
 open Parser
 open Utils
 open Types
@@ -6,42 +5,55 @@ open Types
 type steps = { wire1 : int; wire2 : int }
 
 let point_on_line point line =
-  if point.x = line.p1.x && in_range point.y line.p1.y line.p2.y then true
-  else if point.y = line.p1.y && in_range point.x line.p1.x line.p2.x then true
-  else false
+  (point.x = line.p1.x && in_range point.y line.p1.y line.p2.y)
+  || (point.y = line.p1.y && in_range point.x line.p1.x line.p2.x)
 
 let coll_on_line colls line =
-  let rec helper c =
+  let rec helper c acc =
     match c with
-    | [] -> None
+    | [] -> acc
     | point :: rest ->
-        if point_on_line point line then Some point else helper rest
+        if point_on_line point line then helper rest (point :: acc)
+        else helper rest acc
   in
 
-  helper colls
+  helper colls []
 
-let walk wire colls =
+let coll_dists wire colls =
   let hmap = Hashtbl.create (List.length colls) in
   let rec helper rem dist =
     match rem with
     | [] -> hmap
     | line :: rest ->
-        (match coll_on_line colls line with
-        | Some point -> Hashtbl.add hmap point (dist + man_dist line.p1 point)
-        | None -> ());
+        let points = coll_on_line colls line in
+        let add_map point =
+          Hashtbl.add hmap point (dist + man_dist line.p1 point)
+        in
+
+        List.iter add_map points;
         helper rest (dist + man_dist line.p1 line.p2)
   in
 
   helper wire 0
 
+let combine dists_1 dists_2 =
+  let rec helper rem acc =
+    match rem with
+    | [] -> acc
+    | (p, d) :: rest ->
+        let d' = Hashtbl.find dists_2 p in
+        helper rest ((d + d') :: acc)
+  in
+
+  let dists_1_list = Hashtbl.fold (fun k v acc -> (k, v) :: acc) dists_1 [] in
+
+  helper dists_1_list []
+
 let run file_name =
   let wire1, wire2 = parse_input file_name in
   let colls = collisions wire1 wire2 in
-  let wire1_dists = walk wire1 colls in
-  let wire2_dists = walk wire2 colls in
+  let wire1_dists = coll_dists wire1 colls in
+  let wire2_dists = coll_dists wire2 colls in
+  let combined = combine wire1_dists wire2_dists in
 
-  print_endline "Wire 1";
-  Hashtbl.iter (fun k v -> printf "%d,%d %d\n" k.x k.y v) wire1_dists;
-
-  print_endline "Wire 2";
-  Hashtbl.iter (fun k v -> printf "%d,%d %d\n" k.x k.y v) wire2_dists
+  List.fold_left Stdlib.min Int.max_int combined
