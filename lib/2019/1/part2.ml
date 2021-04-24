43dda6c59830ae8245f7e8a5e7610657801ca4b8
Rich Heller <rellehr@gmail.com>
2021-04-23 23:29:12 -0500

    Code cleanup, add readme

--- lib/2019/1/part2.ml
+++ lib/2019/1/part2.ml
@@ -1,13 +1,13 @@
 open Parser
 open Utils
 
+let rec total_fuel mass =
+  let fuel = fuel_for_mass mass in
+
+  match fuel with _ when fuel <= 0 -> 0 | f -> f + total_fuel f
+
 let run (file_name : string) : int =
   let input = parse_input file_name in
-  let rec total_fuel mass =
-    let fuel = fuel_for_mass mass in
-
-    match fuel with _ when fuel <= 0 -> 0 | f -> f + total_fuel f
-  in
-
   let totals = List.map total_fuel input in
+
   List.fold_left ( + ) 0 totals
--- lib/2019/intcode/intcode.ml
+++ lib/2019/intcode/intcode.ml
@@ -10,21 +10,33 @@ let set_addr m addr value =
 
 let get_addr m addr = m.prog.(addr)
 
-let step m =
-  let op_code, addr_1, addr_2, addr_3 =
-    (m.prog.(m.ndx), m.prog.(m.ndx + 1), m.prog.(m.ndx + 2), m.prog.(m.ndx + 3))
+let op_code_1 m =
+  let addr_1, addr_2, addr_3 =
+    (m.prog.(m.ndx + 1), m.prog.(m.ndx + 2), m.prog.(m.ndx + 3))
   in
 
+  m.prog.(addr_3) <- m.prog.(addr_1) + m.prog.(addr_2);
+  { m with ndx = m.ndx + 4 }
+
+let op_code_2 m =
+  let addr_1, addr_2, addr_3 =
+    (m.prog.(m.ndx + 1), m.prog.(m.ndx + 2), m.prog.(m.ndx + 3))
+  in
+
+  m.prog.(addr_3) <- m.prog.(addr_1) * m.prog.(addr_2);
+  { m with ndx = m.ndx + 4 }
+
+let op_code_99 m = { m with halt = true; ndx = m.ndx + 1 }
+
+let step m =
+  let op_code = m.prog.(m.ndx) in
+
   match op_code with
-  | 1 ->
-      m.prog.(addr_3) <- m.prog.(addr_1) + m.prog.(addr_2);
-      { m with ndx = m.ndx + 4 }
-  | 2 ->
-      m.prog.(addr_3) <- m.prog.(addr_1) * m.prog.(addr_2);
-      { m with ndx = m.ndx + 4 }
-  | 99 -> { m with halt = true; ndx = m.ndx + 1 }
+  | 1 -> op_code_1 m
+  | 2 -> op_code_2 m
+  | 99 -> op_code_99 m
   | _ ->
-      printf "op_code %d %d %d %d\n" op_code addr_1 addr_2 addr_3;
+      printf "unahndled op_code %d\n" op_code;
       m
 
 let rec run_prog m = match m.halt with true -> m | false -> run_prog (step m)
