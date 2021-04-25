b35381a15749cd86a92dcec5df266fa506b016f9
Rich Heller <rellehr@gmail.com>
2021-04-25 18:19:20 -0500

    Permutations function

--- lib/2019/day7/part1.ml
+++ lib/2019/day7/part1.ml
@@ -19,8 +19,44 @@ let run_seq prog seq =
 
   loop seq 0
 
+let swap a n1 n2 =
+  let tmp = a.(n1) in
+  a.(n1) <- a.(n2);
+  a.(n2) <- tmp
+
+let perms amps =
+  let count = ref 0 in
+  let out = ref [] in
+
+  let rec loop size =
+    match size with
+    | 1 ->
+        out := Array.copy amps :: !out;
+        count := !count + 1
+    | size' ->
+        let rec i_loop i stop =
+          if i = stop then ()
+          else (
+            loop (size - 1);
+
+            if size mod 2 = 1 then swap amps 0 (size - 1)
+            else swap amps i (size - 1);
+
+            i_loop (i + 1) stop)
+        in
+
+        i_loop 0 size'
+  in
+
+  loop (Array.length amps);
+
+  !out
+
 let run file_name =
   let prog = Intcode.parse_input file_name in
+
+  let p = perms [| 0; 1; 2; 3; 4 |] in
+  Printf.printf "perms %d\n" (List.length p);
 
   let output = run_seq prog [ 1; 0; 4; 3; 2 ] in
   Printf.printf "output %d\n" output;
