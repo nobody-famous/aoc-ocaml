119c73599ae9c90bce3f2b367c9a333a650cca8e
Rich Heller <rellehr@gmail.com>
2021-05-04 01:05:54 -0500

    Part 1 work

--- lib/2019/14/part1.ml
+++ lib/2019/14/part1.ml
@@ -1,9 +1,70 @@
-let run file_name =
-  let input = Parser.parse_input file_name in
-  let fuel =
-    try Hashtbl.find input "FUEL"
-    with Not_found -> raise @@ Failure "FUEL NOT FOUND"
+let ceil_div num den = if num mod den > 0 then (num / den) + 1 else num / den
+
+let find_min_amount needed required =
+  let mul =
+    if needed mod required > 0 then (needed / required) + 1
+    else needed / required
   in
 
-  Printf.printf "%d %d\n" fuel.amount @@ List.length fuel.chems;
+  mul * required
+
+let sum_chems input =
+  let sums = Hashtbl.create 64 in
+
+  let rec walk (input : (string, Parser.input_chems) Hashtbl.t) name mul =
+    let in_chems =
+      try Hashtbl.find input name
+      with Not_found -> raise @@ Failure (Printf.sprintf "Missing %s" name)
+    in
+
+    Printf.printf "%s %d\n" name mul;
+
+    let top = List.nth in_chems.chems 0 in
+    if top.name = "ORE" then (
+      let cur_value = try Hashtbl.find sums name with Not_found -> 0 in
+      Hashtbl.replace sums name (cur_value + mul);
+      Printf.printf "BOTTOM %s %d\n" name (cur_value + mul))
+    else
+      List.iter
+        (fun (chem : Parser.chemical) ->
+          let mul' = max 1 (ceil_div mul in_chems.amount) in
+
+          Printf.printf "  %s %d %d %d\n" chem.name in_chems.amount chem.amount
+            mul';
+          walk input chem.name (chem.amount * mul'))
+        in_chems.chems
+  in
+
+  walk input "FUEL" 1;
+
+  sums
+
+let find_ores (input : (string, Parser.input_chems) Hashtbl.t) =
+  Hashtbl.fold
+    (fun name (in_chems : Parser.input_chems) acc ->
+      let top : Parser.chemical = List.nth in_chems.chems 0 in
+      if top.name = "ORE" then name :: acc else acc)
+    input []
+
+let run file_name =
+  let input = Parser.parse_input file_name in
+  let ores = find_ores input in
+  let sums = sum_chems input in
+
+  let answer =
+    List.fold_left
+      (fun acc name ->
+        let amount = Hashtbl.find sums name in
+        let required = Hashtbl.find input name in
+        let ore_chem = List.nth required.chems 0 in
+        let min_amount = find_min_amount amount required.amount in
+        let ore_amount = min_amount / required.amount * ore_chem.amount in
+
+        Printf.printf "%s %d %d %d %d %d\n" name amount required.amount
+          ore_chem.amount min_amount ore_amount;
+        acc + ore_amount)
+      0 ores
+  in
+
+  Printf.printf "%d\n" answer;
   0
