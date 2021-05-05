6592dbd6424cb6d717c34f30c676a7e3cd8f5cad
Rich Heller <rellehr@gmail.com>
2021-05-05 09:32:54 -0500

    Code cleanup

--- lib/2019/14/part1.ml
+++ lib/2019/14/part1.ml
@@ -1,45 +1,48 @@
 let ceil_div num den = (num + (den - 1)) / den
 
-let add_to_ht ht key value =
+let inc_ht_entry ht key value =
   let cur_value = try Hashtbl.find ht key with Not_found -> 0 in
   Hashtbl.replace ht key @@ (value + cur_value)
 
-let calc_ore (input : (string, Parser.reaction) Hashtbl.t) =
-  let seen = Hashtbl.create 64 in
-  let req = Hashtbl.create 64 in
-
+let count_parents input num_parents =
   Hashtbl.iter
     (fun _ (v : Parser.reaction) ->
       List.iter
-        (fun (chem : Parser.chemical) -> add_to_ht seen chem.name 1)
+        (fun (chem : Parser.chemical) -> inc_ht_entry num_parents chem.name 1)
         v.chems)
-    input;
+    input
 
-  Hashtbl.replace seen "FUEL" 0;
+let calc_ore input =
+  let parent_counts = Hashtbl.create 64 in
+  let req = Hashtbl.create 64 in
+
+  count_parents input parent_counts;
+
+  Hashtbl.replace parent_counts "FUEL" 0;
   Hashtbl.replace req "FUEL" 1;
 
   let rec loop () =
     let n =
       Hashtbl.fold
-        (fun key value acc ->
-          if value <> 0 then acc
+        (fun name parent_count acc ->
+          if parent_count <> 0 then acc
           else
-            let n = Hashtbl.find req key in
+            let n = Hashtbl.find req name in
 
-            match key with
+            match name with
             | "ORE" -> Some n
             | _ ->
-                (let entry = Hashtbl.find input key in
+                (let entry = Hashtbl.find input name in
                  let amt = ceil_div n entry.amount in
 
                  List.iter
                    (fun (chem : Parser.chemical) ->
-                     add_to_ht req chem.name (amt * chem.amount);
-                     add_to_ht seen chem.name (-1))
+                     inc_ht_entry req chem.name (amt * chem.amount);
+                     inc_ht_entry parent_counts chem.name (-1))
                    entry.chems);
-                Hashtbl.remove seen key;
+                Hashtbl.remove parent_counts name;
                 acc)
-        seen None
+        parent_counts None
     in
 
     match n with None -> loop () | Some value -> value
