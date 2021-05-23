0c4f19f0977ca4021a556c23eb6ed428ea7fda39
Rich Heller <rellehr@gmail.com>
2021-05-23 10:07:42 -0500

    Cleanup day1

--- lib/2019/1/part1.ml
+++ lib/2019/1/part1.ml
@@ -2,6 +2,4 @@ open Parser
 open Utils
 
 let run (file_name : string) : int =
-  let input = parse_input file_name in
-
-  List.fold_left ( + ) 0 (fuel_required input)
+  parse_input file_name |> fuel_required |> List.fold_left ( + ) 0
