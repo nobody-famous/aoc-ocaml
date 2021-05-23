b3642d780da3f4f500cc860421a4315c5b8f8bc1
Rich Heller <rellehr@gmail.com>
2021-05-23 11:10:55 -0500

    Cleanup day13

--- lib/2019/13/part1.ml
+++ lib/2019/13/part1.ml
@@ -1,8 +1,7 @@
 open Utils
 
 let run file_name =
-  let prog = Intcode.parse_input file_name in
-  let game = new_game prog |> run_game in
+  let game = Intcode.parse_input file_name |> new_game |> run_game in
 
   Hashtbl.fold
     (fun _ tile acc -> if tile = BLOCK then acc + 1 else acc)
