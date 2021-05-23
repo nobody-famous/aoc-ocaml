8319914f1ebe88a4b2f1b37fbd040694ca4936e3
Rich Heller <rellehr@gmail.com>
2021-05-23 10:49:34 -0500

    Cleanup day9

--- lib/2019/9/part2.ml
+++ lib/2019/9/part2.ml
@@ -1,8 +1,5 @@
 open Utils
 
 let run file_name =
-  let prog = Intcode.parse_input file_name in
-  let mach = Intcode.new_machine () prog in
-  let mach = Intcode.set_input 2 mach in
-
-  run_mach mach 0
+  Intcode.parse_input file_name
+  |> Intcode.new_machine () |> Intcode.set_input 2 |> run_mach 0
--- lib/2019/9/utils.ml
+++ lib/2019/9/utils.ml
@@ -1,12 +1,12 @@
-let rec run_mach m out =
+let rec run_mach out m =
   let m = Intcode.step m in
 
   match Intcode.get_state m with
   | HALT -> out
-  | RUN -> run_mach m out
+  | RUN -> run_mach out m
   | OUTPUT -> (
       let m, out' = Intcode.get_output m in
-      match out' with Some v -> v | None -> run_mach m 0)
+      match out' with Some v -> v | None -> run_mach 0 m)
   | s ->
       Printf.printf "STATE %s\n" (Intcode.state_to_string s);
       0
