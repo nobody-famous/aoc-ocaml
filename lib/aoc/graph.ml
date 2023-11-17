b57b22762032a6a3a5b4cda7682dc6147244ffe8
Rich Heller <rellehr@gmail.com>
2023-11-16 22:37:57 -0600

    Initialize path frontier

--- lib/aoc/graph.ml
+++ lib/aoc/graph.ml
@@ -2,15 +2,32 @@ type position = { row : int; col : int }
 type ('pos, 'w) edge = { target : 'pos; weight : 'w }
 type ('pos, 'w) node = { edges : ('pos, 'w) edge list }
 type ('pos, 'w) graph = ('pos, ('pos, 'w) node) Hashtbl.t
-type ('pos, 'w) visited_node = { path : 'pos list; weight : 'w }
+type ('pos, 'w) path_node = { path : 'pos list; weight : 'w }
 
 type ('pos, 'w) path_state = {
-  visited : ('pos, ('pos, 'w) visited_node) Hashtbl.t;
-  frontier : ('pos, int) Hashtbl.t;
+  visited : ('pos, ('pos, 'w) path_node) Hashtbl.t;
+  frontier : ('pos, ('pos, 'w) path_node) Hashtbl.t;
 }
 
 let shortest_path ~start_pos:s ~end_pos:e ~init_weight:w graph =
-  let update_frontier pos state = state in
+  let add_to_frontier state pos path weight =
+    match Hashtbl.find_opt state.frontier pos with
+    | Some n when weight < n.weight ->
+        Hashtbl.replace state.frontier pos { path; weight }
+    | Some _ -> ()
+    | None -> Hashtbl.add state.frontier pos { path; weight }
+  in
+
+  let update_frontier state node =
+    node.path
+    |> List.hd
+    |> Hashtbl.find graph
+    |> (fun n -> n.edges)
+    |> List.iter (fun e ->
+           add_to_frontier state e.target (e.target :: node.path) e.weight);
+
+    state
+  in
 
   let create_state visited = { visited; frontier = Hashtbl.create 64 } in
 
@@ -19,9 +36,20 @@ let shortest_path ~start_pos:s ~end_pos:e ~init_weight:w graph =
     |> List.to_seq
     |> Hashtbl.of_seq
     |> create_state
-    |> update_frontier s
   in
 
-  let find_path state = { path = []; weight = w } in
+  let init_frontier state =
+    s |> Hashtbl.find state.visited |> update_frontier state
+  in
 
-  init_state |> find_path
+  let find_path state =
+    Hashtbl.iter
+      (fun key value ->
+        Printf.printf "%d %d [" key value.weight;
+        List.iter (fun p -> Printf.printf "%d " p) value.path;
+        Printf.printf "]\n")
+      state.frontier;
+    { path = []; weight = w }
+  in
+
+  init_state |> init_frontier |> find_path
