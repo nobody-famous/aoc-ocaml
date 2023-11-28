7f923cc0615f366f85e64de6a89e5340e62488c5
Rich Heller <rellehr@gmail.com>
2023-11-27 22:08:29 -0600

    Part 2 done

---
+++ lib/2022/13/part2.ml
@@ -0,0 +1,26 @@
+let div_1 = Utils.PacketList [ Utils.PacketList [ Utils.Integer 2 ] ]
+let div_2 = Utils.PacketList [ Utils.PacketList [ Utils.Integer 6 ] ]
+
+let collapse_groups grps =
+  let break_pair (left, right) = [ left; right ] in
+
+  grps |> List.map break_pair |> List.flatten
+
+let add_dividers packets = div_1 :: div_2 :: packets
+
+let cmp_packets pkt_1 pkt_2 =
+  match Utils.in_order (pkt_1, pkt_2) with
+  | Utils.InOrder -> -1
+  | Utils.OutOfOrder -> 1
+  | Utils.Unknown -> 0
+
+let to_indices index pkt = if pkt = div_1 || pkt = div_2 then index + 1 else 1
+
+let run lines =
+  lines
+  |> Parser.parse_input
+  |> collapse_groups
+  |> add_dividers
+  |> List.sort cmp_packets
+  |> List.mapi to_indices
+  |> List.fold_left ( * ) 1
--- lib/2022/13/utils.ml
+++ lib/2022/13/utils.ml
@@ -1,1 +1,24 @@
 type packet = Integer of int | PacketList of packet list
+type order_result = InOrder | OutOfOrder | Unknown
+
+let rec in_order (pkt_1, pkt_2) =
+  let rec cmp_lists list_1 list_2 =
+    match (list_1, list_2) with
+    | fst_1 :: rest_1, fst_2 :: rest_2 -> (
+        match in_order (fst_1, fst_2) with
+        | InOrder -> InOrder
+        | OutOfOrder -> OutOfOrder
+        | Unknown -> cmp_lists rest_1 rest_2)
+    | [], [] -> Unknown
+    | [], _ -> InOrder
+    | _, [] -> OutOfOrder
+  in
+
+  match (pkt_1, pkt_2) with
+  | PacketList list_1, PacketList list_2 -> cmp_lists list_1 list_2
+  | PacketList list_1, Integer int_2 -> cmp_lists list_1 [ Integer int_2 ]
+  | Integer int_1, PacketList list_2 -> cmp_lists [ Integer int_1 ] list_2
+  | Integer left, Integer right ->
+      if left < right then InOrder
+      else if right < left then OutOfOrder
+      else Unknown
