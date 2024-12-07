aa9f4221a3805befab3754a1b26680d4d6a7fd20
Rich Heller <rellehr@gmail.com>
2024-12-07 14:49:22 -0600

    Code cleanup

--- lib/2024/6/part1.ml
+++ lib/2024/6/part1.ml
@@ -10,23 +10,21 @@ let find_start grid =
   Hashtbl.fold
     (fun k v acc ->
       match acc with
-      | Some v, g -> (Some v, g)
-      | None, g -> if v = '^' then (Some k, g) else (None, g))
+      | Some _, _ -> acc
+      | None, g -> if v = '^' then (Some k, g) else acc)
     grid (None, grid)
 
 let rec do_steps dir (row, col) grid path =
-  (match Hashtbl.find_opt path (row, col) with
-  | None -> Hashtbl.replace path (row, col) true
-  | Some _ -> ());
+  let new_path = Utils.Points.add (row, col) path in
 
   let handle_step new_row new_col grid =
     match Hashtbl.find_opt grid (new_row, new_col) with
-    | None -> path
+    | None -> new_path
     | Some v ->
         if v = '#' then
-          do_steps (turn_right dir) (row, col) grid path
+          do_steps (turn_right dir) (row, col) grid new_path
         else
-          do_steps dir (new_row, new_col) grid path
+          do_steps dir (new_row, new_col) grid new_path
   in
 
   match dir with
@@ -37,7 +35,7 @@ let rec do_steps dir (row, col) grid path =
 
 let walk_path (start, grid) =
   match start with
-  | None -> Hashtbl.create 16
-  | Some (row, col) -> do_steps Up (row, col) grid (Hashtbl.create 16)
+  | None -> Utils.Points.empty
+  | Some (row, col) -> do_steps Up (row, col) grid Utils.Points.empty
 
-let run lines = lines |> Parser.parse |> find_start |> walk_path |> Hashtbl.length |> fun len -> Aoc.Utils.IntResult len
+let run lines = Aoc.Utils.IntResult (lines |> Parser.parse |> find_start |> walk_path |> Utils.Points.cardinal)
---
+++ lib/2024/6/utils.ml
@@ -0,0 +1,8 @@
+module Point = struct
+  type t = int * int
+
+  let compare (a_row, a_col) (b_row, b_col) =
+    if a_row < b_row || (a_row = b_row && a_col < b_col) then -1 else if a_row = b_row && a_col = b_col then 0 else 1
+end
+
+module Points = Set.Make (Point)
