053922d5025e007e77d732d73a180d132f937955
Rich Heller <rellehr@gmail.com>
2024-12-14 13:58:35 -0600

    Test cleanup

--- lib/2024/6/test.ml
+++ lib/2024/6/test.ml
@@ -1,7 +1,7 @@
 open OUnit2
 open TestUtils
 
-let test_data =
+let lines =
   [
     "....#.....";
     ".........#";
@@ -17,11 +17,11 @@ let test_data =
 
 let part1_tests =
   "2024 Day 6 Part 1"
-  >::: [ ("Sample" >:: fun _ -> test_data |> Aoc_2024_6.Part1.run |> check_equal @@ Aoc.Utils.IntResult 41) ]
+  >::: [ ("Sample" >:: fun _ -> lines |> Aoc_2024_6.Part1.run |> check_equal @@ Aoc.Utils.IntResult 41) ]
 
 let part2_tests =
   "2024 Day 6 Part 2"
-  >::: [ ("Sample" >:: fun _ -> test_data |> Aoc_2024_6.Part2.run |> check_equal @@ Aoc.Utils.IntResult 6) ]
+  >::: [ ("Sample" >:: fun _ -> lines |> Aoc_2024_6.Part2.run |> check_equal @@ Aoc.Utils.IntResult 6) ]
 
 let _ = run_test_tt_main part1_tests
 let _ = run_test_tt_main part2_tests
