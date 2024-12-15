4d5b9fc63168ed8e472309923aac96dcda74d17a
Rich Heller <rellehr@gmail.com>
2024-12-15 14:05:34 -0600

    Day 7, part 2 performance fix

--- lib/2024/7/part1.ml
+++ lib/2024/7/part1.ml
@@ -1,6 +1,9 @@
-let rec matches_value target numbers cur_value =
+let rec matches_numbers target numbers cur_value =
   match numbers with
-  | number :: rest -> matches_value target rest (cur_value + number) || matches_value target rest (cur_value * number)
-  | [] -> target = cur_value
+  | [] -> target = 0
+  | number :: rest ->
+      (target mod number = 0 && matches_numbers (target / number) rest cur_value)
+      || (target - number >= 0 && matches_numbers (target - number) rest cur_value)
 
+let matches_value target numbers cur_value = matches_numbers target (List.rev numbers) cur_value
 let run lines = Utils.solve matches_value lines
--- lib/2024/7/part2.ml
+++ lib/2024/7/part2.ml
@@ -1,11 +1,17 @@
-let concat_ints a b = string_of_int a ^ string_of_int b |> int_of_string
+let check_ends_with a b = String.ends_with ~suffix:(string_of_int b) (string_of_int a)
 
-let rec matches_value target numbers cur_value =
+let strip_suffix a b =
+  let a_str = string_of_int a in
+  let b_str = string_of_int b in
+  int_of_string @@ String.sub a_str 0 (String.length a_str - String.length b_str)
+
+let rec matches_numbers target numbers cur_value =
   match numbers with
-  | [] -> target = cur_value
+  | [] -> target = 0
   | number :: rest ->
-      matches_value target rest (cur_value + number)
-      || matches_value target rest (cur_value * number)
-      || matches_value target rest (concat_ints cur_value number)
+      (target mod number = 0 && matches_numbers (target / number) rest cur_value)
+      || (target - number >= 0 && matches_numbers (target - number) rest cur_value)
+      || (check_ends_with target number && matches_numbers (strip_suffix target number) rest cur_value)
 
+let matches_value target numbers cur_value = matches_numbers target (List.rev numbers) cur_value
 let run lines = Utils.solve matches_value lines
