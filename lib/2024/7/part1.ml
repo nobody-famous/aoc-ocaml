3b226c7f1918c5ee838d552a1f7f388b63596ef3
Rich Heller <rellehr@gmail.com>
2024-12-14 22:34:47 -0600

    Day 7, part 1

--- lib/2024/7/part1.ml
+++ lib/2024/7/part1.ml
@@ -1,2 +1,8 @@
-let get_answer _ = Aoc.Utils.IntResult 0
+let rec matches_value target numbers cur_value =
+  match numbers with
+  | number :: rest -> matches_value target rest (cur_value + number) || matches_value target rest (cur_value * number)
+  | [] -> target = cur_value
+
+let eq_is_true (target, numbers) = matches_value target numbers 0
+let get_answer eqs = Aoc.Utils.IntResult (eqs |> List.filter eq_is_true |> List.fold_left (fun acc (v, _) -> acc + v) 0)
 let run lines = lines |> Parser.parse_input |> get_answer
