b6e43a07b6a5f6758379e791b23a640acf68ccdd
Rich Heller <rellehr@gmail.com>
2024-12-21 17:47:27 -0600

    Still working on it

--- lib/2024/8/part1.ml
+++ lib/2024/8/part1.ml
@@ -19,15 +19,35 @@ let find_frequencies grid =
 
   frequencies
 
-let gen_pairs pts = pts
+let rec create_pairs pt rest =
+  match rest with
+  | first :: rest -> (pt, first) :: create_pairs pt rest
+  | [] -> []
+
+let rec zip = function
+  | first :: rest -> List.concat [ create_pairs first rest; zip rest ]
+  | [] -> []
+
+(* let gen_pairs (pts : (int * int, bool) Hashtbl.t) = *)
+let gen_pairs pts = Hashtbl.to_seq pts |> List.of_seq |> List.map (fun (k, _) -> k) |> zip
 
 let build_pairs frequencies =
   let pairs = Hashtbl.create 16 in
 
   Hashtbl.iter (fun freq pts -> Hashtbl.replace pairs freq @@ gen_pairs pts) frequencies;
 
-  frequencies
+  pairs |> Hashtbl.to_seq |> List.of_seq |> List.map (fun (_, v) -> v) |> List.concat
+
+let unzip (a, b) = [ a; b ]
+
+let pair_antinodes (left, right) =
+  let row_diff = fst left - fst right in
+  let col_diff = snd left - snd right in
+  [ (left, right); (left, right) ]
+
+let get_antinodes pairs = pairs |> List.map pair_antinodes |> List.flatten
 
 let run lines =
-  let _ = Parser.parse_input lines |> find_frequencies |> build_pairs in
+  let tmp = Parser.parse_input lines |> find_frequencies |> build_pairs |> get_antinodes in
+  Printf.printf "***** TMP %d\n" @@ List.length tmp;
   Aoc.Utils.IntResult 0
