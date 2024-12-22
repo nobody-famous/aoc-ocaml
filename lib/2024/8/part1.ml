17ca5797e44fab4c54729b2fba890814054b7cd2
Rich Heller <rellehr@gmail.com>
2024-12-21 22:14:40 -0600

    Day 8, part 1

--- lib/2024/8/part1.ml
+++ lib/2024/8/part1.ml
@@ -17,7 +17,7 @@ let find_frequencies grid =
     done
   done;
 
-  frequencies
+  (grid, frequencies)
 
 let rec create_pairs pt rest =
   match rest with
@@ -28,26 +28,32 @@ let rec zip = function
   | first :: rest -> List.concat [ create_pairs first rest; zip rest ]
   | [] -> []
 
-(* let gen_pairs (pts : (int * int, bool) Hashtbl.t) = *)
 let gen_pairs pts = Hashtbl.to_seq pts |> List.of_seq |> List.map (fun (k, _) -> k) |> zip
+let to_tuple x y = (x, y)
 
-let build_pairs frequencies =
+let build_pairs (grid, frequencies) =
   let pairs = Hashtbl.create 16 in
 
   Hashtbl.iter (fun freq pts -> Hashtbl.replace pairs freq @@ gen_pairs pts) frequencies;
 
-  pairs |> Hashtbl.to_seq |> List.of_seq |> List.map (fun (_, v) -> v) |> List.concat
+  pairs |> Hashtbl.to_seq |> List.of_seq |> List.map (fun (_, v) -> v) |> List.concat |> to_tuple grid
 
 let unzip (a, b) = [ a; b ]
+let add_pt (left_row, left_col) (right_row, right_col) = (left_row + right_row, left_col + right_col)
+let sub_pt (left_row, left_col) (right_row, right_col) = (left_row - right_row, left_col - right_col)
 
 let pair_antinodes (left, right) =
-  let row_diff = fst left - fst right in
-  let col_diff = snd left - snd right in
-  [ (left, right); (left, right) ]
+  let diff = (fst left - fst right, snd left - snd right) in
+  [ add_pt left diff; sub_pt right diff ]
 
-let get_antinodes pairs = pairs |> List.map pair_antinodes |> List.flatten
+let get_antinodes (grid, pairs) = pairs |> List.map pair_antinodes |> List.flatten |> to_tuple grid
+let on_grid row col grid = row >= 0 && row < Array.length grid && col >= 0 && col < Array.length grid.(0)
+
+let filter_nodes (grid, nodes) =
+  let filtered = Hashtbl.create 16 in
+  List.iter (fun (row, col) -> if on_grid row col grid then Hashtbl.replace filtered (row, col) true) nodes;
+  filtered |> Hashtbl.to_seq_keys |> List.of_seq
 
 let run lines =
-  let tmp = Parser.parse_input lines |> find_frequencies |> build_pairs |> get_antinodes in
-  Printf.printf "***** TMP %d\n" @@ List.length tmp;
-  Aoc.Utils.IntResult 0
+  Aoc.Utils.IntResult
+    (Parser.parse_input lines |> find_frequencies |> build_pairs |> get_antinodes |> filter_nodes |> List.length)
